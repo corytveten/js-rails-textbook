ca6d2e54408c614db55bd6df48ebf2b0994a87c9
corytveten <corytveten@gmail.com>
2019-03-12 18:32:36 -0700

    controller stubs

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,24 @@
 class CoursesController < ApplicationController
+    def show
+        @course = Course.find(params[:id])
+    end
+
+    def index
+        @courses = Course.all
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+        course = Course.create(course_params)
+        redirect_to course
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:title, :code)
+    end
 end
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,24 @@
 class SchoolsController < ApplicationController
   def show
+    @school = School.find(params[:id])
+  end
+
+  def index
+    @schools = School.all
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    school = School.create(school_params)
+    redirect_to school
+  end
+
+private
+
+  def school_params
+    params.require(:school).permit(:title)
   end
 end
