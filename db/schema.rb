0ce91aca226a10d69a96175d4a3be5ddb191f0ca
corytveten <corytveten@gmail.com>
2019-03-20 17:57:00 -0700

    add uid column and gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
 
   create_table "course_textbooks", force: :cascade do |t|
     t.integer "course_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 5) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
   end
 
 end
