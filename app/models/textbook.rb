7c7d781b01274e060af9e18e2bf79d1722c7a902
corytveten <corytveten@gmail.com>
2019-03-26 20:06:29 -0700

    textbook pages updates

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -3,13 +3,6 @@ class Textbook < ApplicationRecord
     has_many :courses, through: :course_textbooks
 
     validates :title, presence: true
-    validates :course_id, presence: true
+    validates :author, presence: true
 
-    def course_code=(code)
-        self.courses = Course.find_or_create_by(code: code)
-    end
-
-    def course_code
-        self.courses ? self.courses.code : nil
-    end
 end
