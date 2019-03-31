c470ea60be2f8aff81dbe6ceb8c3c9577c414039
corytveten <corytveten@gmail.com>
2019-03-31 12:23:36 -0700

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
     validates :username, presence: true
-    validates :email, presence: true #uniqueness: true
-    #validates :password, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
     has_secure_password
 
 
