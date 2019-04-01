05b3d437fe374e1d86b13d85c52ebf9df214e06c
corytveten <corytveten@gmail.com>
2019-03-31 21:12:23 -0700

    correct course_textbook controller

--- app/controllers/course_textbooks_controller.rb
+++ app/controllers/course_textbooks_controller.rb
@@ -16,6 +16,7 @@ before_action :logged_in?, :login_required
         @course = Course.find_by(id: params[:course_id])
         @textbook = Textbook.find_by(id: params[:textbook_id])
         @course_textbook = @textbook.course_textbooks.new(course_textbook_params)
+        @course_textbook.course_id = @course.id
         #raise params.inspect
         if @course_textbook.save
             redirect_to course_path(@course)
